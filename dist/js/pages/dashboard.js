6aef2c1f4470a448a744af5364ae0e97a90f4275
Rahul Dani <rhl.dani80@gmail.com>
2021-07-16 19:24:26 +0530

    data text changes

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -208,7 +208,7 @@ $(function () {
   // $('#revenue-chart').get(0).getContext('2d');
 
   var salesGraphChartData = {
-    labels: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2', '2012 Q3', '2012 Q4', '2013 Q1', '2013 Q2'],
+    labels: ['APRA', 'IPRS', 'COMPASS', 'AGADU', 'AMRA', 'ALBAUTOR', 'FILSCAP', 'GEMA'],
     datasets: [
       {
         label: 'Digital Goods',
@@ -221,7 +221,7 @@ $(function () {
         pointHoverRadius: 7,
         pointColor: '#efefef',
         pointBackgroundColor: '#efefef',
-        data: [2666, 2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432]
+        data: [2778, 4912, 3767, 6810, 5670, 4820, 15073, 10687, 8432]
       }
     ]
   }
